cb41c93e7121e375e76b46facbec040a58d84043
Limberg Montes <limberg.montes@agetic.gob.bo>
2024-03-18 17:20:56 -0400

    feat: se implemento swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config'
+import { SWAGGER_API_CURRENT_VERSION, SWAGGER_API_DESCRIPTION, SWAGGER_API_NAME, SWAGGER_API_ROOT } from './common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
 export const SessionAppDataSource = new DataSource({
   type: 'mysql',
@@ -24,6 +27,9 @@ const bootstrap = async () =>{
   //Configuracion de la base de datos
   await SessionAppDataSource.initialize()
   const configService = app.get(ConfigService)
+  if (configService.get('NODE_ENV') !== 'production') {
+    createSwagger(app)
+  }
   // Configuracion de swagger
     app.setGlobalPrefix(configService.get('PATH_SUBDOMAIN') || 'api')
     app.enableCors({
@@ -35,4 +41,19 @@ const bootstrap = async () =>{
   await app.listen(port)
 
 }
+
+
+function createSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle(SWAGGER_API_NAME)
+    .setDescription(SWAGGER_API_DESCRIPTION)
+    .setVersion(SWAGGER_API_CURRENT_VERSION)
+    .addServer(`http://localhost:${process.env.PORT}/api/`)
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup(SWAGGER_API_ROOT, app, document)
+}
+
 bootstrap();
