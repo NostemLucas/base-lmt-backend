ea127f58616c169a920374c9e18a49feee34cfa7
Limberg Montes <limberg.montes@agetic.gob.bo>
2024-03-18 17:07:30 -0400

    feat: se implemento typeorm y conexion con bd mysql

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+export const SessionAppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  synchronize: false,
+  entities: [__dirname + '/../src/**/*.entity{.ts,.js}'],
+})
+
+const bootstrap = async () =>{
+ //Configuracion de la aplicacion de nestjs
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  })
+
+
+  //Configuracion de la base de datos
+  await SessionAppDataSource.initialize()
+  const configService = app.get(ConfigService)
+  // Configuracion de swagger
+    app.setGlobalPrefix(configService.get('PATH_SUBDOMAIN') || 'api')
+    app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
+  const port = configService.get('PORT')
+  await app.listen(port)
+
 }
 bootstrap();
